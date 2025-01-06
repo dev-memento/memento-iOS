760591609daabd5278a51271dbb2992647ecbe20
mcrkgus <mcrkgus@naver.com>
2025-01-06 15:22:00 +0900

    [Setting] #1 - foldering

---
+++ Memento-iOS/Memento-iOS/Source/Network/Base/BaseAPIService.swift
@@ -0,0 +1,12 @@
+//
+//  BaseAPIService.swift
+//  Memento-iOS
+//
+//  Created by Gahyun Kim on 1/6/25.
+//
+
+import Foundation
+
+class BaseAPIService {
+    // TODO: - statusCode에 따라 decoding 함수
+}
---
+++ Memento-iOS/Memento-iOS/Source/Network/Base/NetworkService.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkService.swift
+//  Memento-iOS
+//
+//  Created by Gahyun Kim on 1/6/25.
+//
+
+import Foundation
+
+final class NetworkService {
+    
+    static let shared = NetworkService()
+    
+    private init() {}
+    
+//    let todayService: TodayRepositoryProtocol = TodayRepository()
+    
+}
