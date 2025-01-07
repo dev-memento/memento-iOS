8ba6b599009aa75c84129df7ef5e2e5d92021973
mcrkgus <mcrkgus@naver.com>
2025-01-07 21:11:09 +0900

    [Feat] #2 - TabBarView

---
+++ Memento-iOS/Memento-iOS/Source/Component/TabBar/TabBarView.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarView.swift
+//  Memento-iOS
+//
+//  Created by Gahyun Kim on 1/7/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    @State private var selectedTab: TabBarItem = .today
+    
+    init() {
+        UITabBar.appearance().backgroundColor = .darkGray
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            TabView(selection: $selectedTab) {
+                ForEach(TabBarItem.allCases, id: \.self) { tabItem in
+                    tabItem.targetView
+                        .tabItem {
+                            VStack {
+                                selectedTab == tabItem ? tabItem.selectedItem : tabItem.normalItem
+                            }
+                        }
+                        .tag(tabItem)
+                }
+            }
+            .accentColor(.white)
+        }
+    }
+    
+}
