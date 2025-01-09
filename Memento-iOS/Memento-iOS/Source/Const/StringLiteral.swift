e8e0cf9e4e943311ddcfe170e5015a0ed4e6e4dc
mcrkgus <mcrkgus@naver.com>
2025-01-10 01:24:00 +0900

    [Add] #8 - StringLiterals

---
+++ Memento-iOS/Memento-iOS/Source/Const/StringLiteral.swift
@@ -0,0 +1,20 @@
+//
+//  StringLiteral.swift
+//  Memento-iOS
+//
+//  Created by Gahyun Kim on 1/10/25.
+//
+
+import Foundation
+
+enum StringLiteral {
+    
+    enum Priority {
+        static let immediate = "Immediate"
+        static let high = "High"
+        static let medium = "Medium"
+        static let low = "Low"
+        static let none = "None"
+    }
+    
+}
