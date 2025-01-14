28aa2973399cac13df90ef180f8625c446a9a9cd
정정욱 <jeonguk29@naver.com>
2025-01-15 03:04:46 +0900

    [Feat] #26 - Launch Screen 구현

--- Memento-iOS/Memento-iOS/Source/App/Memento_iOSApp.swift
+++ Memento-iOS/Memento-iOS/Source/App/Memento_iOSApp.swift
@@ -12,6 +12,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         registerFonts()
+        Thread.sleep(forTimeInterval: 2)
         return true
     }
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
