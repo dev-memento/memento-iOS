06259071dab0ce531671154bf29c827c5823e85a
sem-git <lsmsm0327@naver.com>
2025-01-14 17:38:54 +0900

    [Refactor] #15 - 뷰 구조 분리

--- Memento-iOS/Memento-iOS/Source/Component/MementoCell/ScheduleListCell.swift
+++ Memento-iOS/Memento-iOS/Source/Component/MementoCell/ScheduleListCell.swift
@@ -18,26 +18,11 @@ struct ScheduleListCell: View {
         HStack(spacing: 10) {
             ColorTagView(colorType: colorType)
             
-            VStack{
-                Image(.ic_event)
-                
-                Spacer()
-            }
-            .padding(.top, 11)
+            IconView()
             
             VStack(alignment: .leading, spacing: 8) {
-                Text(title)
-                    .applyFont(.body_b_16)
-                    .foregroundColor(Color.grayWhite)
-                HStack(spacing: 0) {
-                    Image(.img_notion)
-                    
-                    Text(time)
-                        .applyFont(.detail_r_12)
-                        .foregroundColor(Color.gray05)
-                        .padding(.leading, 12)
-                    
-                }
+                TitleView(title: title)
+                TimeInfoView(time: time)
             }
             
             Spacer()
@@ -47,39 +32,38 @@ struct ScheduleListCell: View {
     }
 }
 
+struct IconView: View {
+    var body: some View {
+        VStack {
+            Image(.ic_event)
+            Spacer()
+        }
+        .padding(.top, 11)
+    }
+}
 
-//struct IconView: View {
-//    var systemName: String
-//    var size: CGFloat
-//
-//    var body: some View {
-//        Image(systemName: systemName)
-//            .resizable()
-//            .frame(width: size, height: size)
-//            .foregroundColor(.white)
-//    }
-//}
+struct TitleView: View {
+    var title: String
+    
+    var body: some View {
+        
+        Text(title)
+            .applyFont(.body_b_16)
+            .foregroundColor(Color.grayWhite)
+        
+    }
+}
 
-//struct TitleView: View {
-//    var title: String
-//
-//    var body: some View {
-//        Text(title)
-//            .applyFont(.body_b_16)
-//            .foregroundColor(Color.grayWhite)
-//    }
-//}
-
-//struct TimeInfoView: View {
-//    var systemImageName: String
-//    var timeText: String
-//
-//    var body: some View {
-//        HStack(spacing: 12) {
-//            IconView(systemName: systemImageName, size: 14)
-//            Text(timeText)
-//                .font(.caption)
-//                .foregroundColor(.gray)
-//        }
-//    }
-//}
+struct TimeInfoView: View {
+    var time: String
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Image(.img_notion)
+            Text(time)
+                .applyFont(.detail_r_12)
+                .foregroundColor(Color.gray05)
+                .padding(.leading, 12)
+        }
+    }
+}
