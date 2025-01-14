4133624f6e83a4a655f25be00393e0c5a3261115
Gahyun Kim <mcrkgus@naver.com>
2025-01-15 04:39:51 +0900

    [Feat] #22 - All day data model

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Today/Model/AllDayDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  AllDayDataModel.swift
+//  Memento-iOS
+//
+//  Created by Kimgahyun on 1/15/25.
+//
+
+import Foundation
+
+struct AllDayDataModel {
+    let colorType: String
+    let text: String
+}
