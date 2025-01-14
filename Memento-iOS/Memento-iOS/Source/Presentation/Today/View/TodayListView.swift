bc3fede311d890afc8b078fa9ab9356cebf3cc1a
sem-git <lsmsm0327@naver.com>
2025-01-15 03:13:59 +0900

    [Fix] #15 - TodayListViewModel 완료 상태 관리 로직 수정

--- Memento-iOS/Memento-iOS/Source/Presentation/Today/View/TodayListView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Today/View/TodayListView.swift
@@ -22,63 +22,42 @@ struct TodayListView: View {
         }
         .background(Color.black)
     }
-    
-    private func renderItem(at index: Int) -> some View {
-        let isHighlighted: Bool
-        
-        
-        if case .todo(let todo) = items[index], !todo.isChecked {
-            
-            isHighlighted = items.prefix(index + 1).filter { if case .todo(let todo) = $0, !todo.isChecked { return true } else { return false } }.count == 1
-        } else {
-            isHighlighted = false
-        }
-        
-        return TodayListItemView(item: $items[index], isHighlighted: isHighlighted)
-        // 드래그 시작 시 draggedItem 업데이트
-            .onDrag {
-                self.draggedItem = items[index]
-                return NSItemProvider()
-}
 
-private extension TodayListView {
-    func renderItem(at index: Int) -> some View {
+    private func renderItem(at index: Int) -> some View {
         let currentItem = viewModel.items[index]
-        let isDragging = currentItem.id == viewModel.dragItem?.id
-        
-        return Group {
-            switch currentItem {
-            case .todo(let todo):
-                TodayListItemView(item: $viewModel.items[index])
-                // 드래그 시작
-                    .onDrag {
-                        viewModel.dragItem = currentItem
-                        return NSItemProvider()
+        let isHighlighted: Bool = {
+            if case .todo(let todo) = currentItem, !todo.isChecked {
+                return viewModel.items.prefix(index + 1)
+                    .filter {
+                        if case .todo(let t) = $0, !t.isChecked { return true }
+                        return false
                     }
-                // 드롭 시작
-                    .onDrop(
-                        of: [.text],
-                        delegate: DropViewDelegate(
-                            item: $viewModel.items[index],
-                            items: $viewModel.items,
-                            draggedItem: $viewModel.dragItem,
-                            dropIndex: $viewModel.dropIndex,
-                            onDrop: viewModel.dropAction
-                        )
-                    )
-            case .schedule(let schedule):
-                TodayListItemView(item: $viewModel.items[index])
+                    .count == 1
             }
-        }
+            return false
+        }()
+
+        return TodayListItemView(item: $viewModel.items[index], isHighlighted: isHighlighted)
+            .onDrag {
+                viewModel.dragItem = currentItem
+                return NSItemProvider()
+            }
+            .onDrop(
+                of: [.text],
+                delegate: DropViewDelegate(
+                    item: $viewModel.items[index],
+                    items: $viewModel.items,
+                    draggedItem: $viewModel.dragItem,
+                    onDrop: viewModel.dropAction
+                )
+            )
     }
 }
 
 struct TodayListItemView: View {
-    
-    @Binding var item: TodayItem
-    var isHighlighted: Bool
     @Binding var item: TodayItemDataModel
-    
+    var isHighlighted: Bool
+
     var body: some View {
         switch item {
         case .todo(let todo):
@@ -101,41 +80,26 @@ struct TodayListItemView: View {
     }
 }
 
-// MARK: - DropViewDelegate
-
 struct DropViewDelegate: DropDelegate {
-    
     @Binding var item: TodayItemDataModel
     @Binding var items: [TodayItemDataModel]
     @Binding var draggedItem: TodayItemDataModel?
-    @Binding var dropIndex: Int?
-    
+
     let onDrop: (TodayItemDataModel?, TodayItemDataModel) -> Void
 
     func dropUpdated(info: DropInfo) -> DropProposal? {
         DropProposal(operation: .move)
     }
 
-    // 드롭 완료
     func performDrop(info: DropInfo) -> Bool {
         withAnimation {
             draggedItem = nil
-            dropIndex = nil
         }
         return true
     }
 
-    // 드롭 대상에 진입
     func dropEntered(info: DropInfo) {
         guard let draggedItem else { return }
         onDrop(draggedItem, item)
-        dropIndex = items.firstIndex { $0.id == item.id }
-    }
-
-    // 드롭 대상에서 벗어났을 때
-    func dropExited(info: DropInfo) {
-        withAnimation {
-            dropIndex = nil
-        }
     }
 }
