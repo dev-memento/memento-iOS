fc605d8c8924cf83fdc727b36729de164acff08f
정정욱 <jeonguk29@naver.com>
2025-01-17 18:53:45 +0900

    [Chore] #37 - NeonAnimationView Component화

--- Memento-iOS/Memento-iOS/Source/Component/NeonAnimationView.swift
+++ Memento-iOS/Memento-iOS/Source/Component/NeonAnimationView.swift
@@ -4,19 +4,22 @@
 //
 //  Created by 정정욱 on 1/17/25.
 //
+
 import SwiftUI
 
 struct NeonAnimationView: View {
     
     @State private var gradientColors: [Color] = [Color.purple, Color.blue, Color.green]
     @State private var lightBrightness: Double = 0.5
+    
+    let width: CGFloat
+    let height: CGFloat
 
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
 
             // 네온 효과 사각형
-            // repeatForever(autoreverses: true)로 애니메이션이 끝나면 반대로 진행
             Rectangle()
                 .strokeBorder(
                     LinearGradient(
@@ -26,26 +29,18 @@ struct NeonAnimationView: View {
                     ),
                     lineWidth: 8
                 )
-                .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.height * 0.90)
-                .shadow(color: Color.purple.opacity(lightBrightness), radius: 20)
-                .shadow(color: Color.blue.opacity(lightBrightness), radius: 30)
+                .frame(width: width, height: height)
+                .shadow(color: gradientColors.first?.opacity(lightBrightness) ?? Color.white.opacity(lightBrightness), radius: 20)
+                .shadow(color: gradientColors.last?.opacity(lightBrightness) ?? Color.white.opacity(lightBrightness), radius: 30)
                 .onAppear {
                     withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
                         lightBrightness = 1.0
                     }
                 }
                 .blur(radius: 1.5)
-
                 .onAppear {
                     startColorAnimation()
                 }
-
-            VStack {
-                Text("Content View")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                Spacer()
-            }
         }
     }
 
@@ -53,14 +48,24 @@ struct NeonAnimationView: View {
         // 타이머로 색상을 일정 시간마다 변경
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
             withAnimation(.easeInOut(duration: 0.5)) {
-                gradientColors = gradientColors.shuffled()
+                // 색상을 랜덤하게 섞음
+                let shuffledColors = gradientColors.shuffled()
+                gradientColors = shuffledColors
             }
         }
     }
 }
 
-struct NeonRectangleView_Previews: PreviewProvider {
-    static var previews: some View {
-        NeonAnimationView()
-    }
+#Preview {
+    NeonAnimationView(
+        width: UIScreen.main.bounds.width * 0.95,
+        height: UIScreen.main.bounds.height * 0.90
+    )
+}
+
+#Preview {
+    NeonAnimationView(
+        width: 343,
+        height: 172
+    )
 }
