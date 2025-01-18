6d5f60cea027910a3234607c9ae4b3d9204b98e3
RAFA <rafa71802937@gmail.com>
2025-01-18 17:30:19 +0900

    [Refactor] #51 - 바텀 시트 종류마다 Detent 값을 동적으로 설정하도록 수정

--- Memento-iOS/Memento-iOS/Source/Utils/DynamicPresentationDetent.swift
+++ Memento-iOS/Memento-iOS/Source/Utils/DynamicPresentationDetent.swift
@@ -11,19 +11,36 @@ import SwiftUI
 struct DynamicPresentationDetent {
 
     /// 기기 화면 크기에 따라 동적으로 Detent 값을 반환
-    static func dynamicDetent() -> Set<PresentationDetent> {
+    static func dynamicDetent(
+        for type: PickerButtonType
+    ) -> Set<PresentationDetent> {
         let screenHeight = UIScreen.main.bounds.height
         let detentHeight: CGFloat
 
-        switch screenHeight {
-        case 0..<668:          // 작은 기기
-            detentHeight = 0.6
-        case 668..<875:        // 중간 기기
-            detentHeight = 0.53
-        case 875..<957:        // 대형 기기
-            detentHeight = 0.5
-        default:               // 그 외 모든 기기
-            detentHeight = 0.6
+        switch type {
+        case .date:
+            switch screenHeight {
+            case 0..<668:          // 작은 기기
+                detentHeight = 0.6
+            case 668..<875:        // 중간 기기
+                detentHeight = 0.53
+            case 875..<957:        // 대형 기기
+                detentHeight = 0.5
+            default:
+                detentHeight = 0.6
+            }
+
+        case .time, .repeat, .endRepeat, .tag:
+            switch screenHeight {
+            case 0..<668:
+                detentHeight = 0.43
+            case 668..<875:
+                detentHeight = 0.36
+            case 875..<957:
+                detentHeight = 0.33
+            default:
+                detentHeight = 0.43
+            }
         }
 
         return [.fraction(detentHeight)]
