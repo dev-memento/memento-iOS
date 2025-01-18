427b9e9d6556c8a0224ebda5813f421b0c1a0e8f
RAFA <rafa71802937@gmail.com>
2025-01-18 16:54:16 +0900

    [Refactor] #51 - DateTimeSectionType 파일명 수정 및 섹션 추가

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddSchedule/Model/AddScheduleSectionType.swift
@@ -0,0 +1,12 @@
+//
+//  AddScheduleSectionType.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 1/18/25.
+//
+
+import Foundation
+
+enum AddScheduleSectionType {
+    case start, end, `repeat`, endRepeat
+}
