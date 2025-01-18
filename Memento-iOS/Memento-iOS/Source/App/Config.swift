b633f01f7131ec46c5d9265556aab31f5acc0737
Gahyun Kim <mcrkgus@naver.com>
2025-01-19 07:50:33 +0900

    [Setting] #58 - Config setting

--- Memento-iOS/Memento-iOS/Source/App/Config.swift
+++ Memento-iOS/Memento-iOS/Source/App/Config.swift
@@ -8,7 +8,25 @@
 import Foundation
 
 enum Config {
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+        }
+    }
     
-    // baseURL
-    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found !!!")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("🍞⛔️BASE_URL is not set in plist for this configuration⛔️🍞")
+        }
+        return key
+    }()
 }
