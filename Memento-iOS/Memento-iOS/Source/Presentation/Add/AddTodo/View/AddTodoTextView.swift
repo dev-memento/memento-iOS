a229ee5517fd52a8131f83c25f4cece11519083e
RAFA <rafa71802937@gmail.com>
2025-01-18 22:23:16 +0900

    [Feat] #55 - 일정 추가 텍스트 뷰 구현

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoTextView.swift
@@ -0,0 +1,37 @@
+//
+//  AddTodoTextView.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 1/18/25.
+//
+
+import SwiftUI
+
+import MDSKit
+
+struct AddTodoTextView: View {
+
+    // MARK: - Properties
+
+    @ObservedObject var viewModel: AddTodoTextViewModel
+
+    // MARK: - Body
+
+    var body: some View {
+        TextField("", text: $viewModel.text, axis: .vertical)
+            .applyFont(.body_b_16)
+            .tint(.mainGreen)
+            .foregroundStyle(Color.grayWhite)
+            .lineLimit(nil)
+            .textFieldStyle(.plain)
+            .onChange(of: viewModel.text) { _, newText in
+                viewModel.limitTextLength(newText)
+            }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    AddTodoTextView(viewModel: AddTodoTextViewModel())
+}
