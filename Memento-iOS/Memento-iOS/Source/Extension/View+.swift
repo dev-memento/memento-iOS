7a28ec9edf4f94623fc2b31c63c11f6adcc5bd70
RAFA <rafa71802937@gmail.com>
2025-01-18 18:24:57 +0900

    [Refactor] #51 - 함수명 더 명확하게 수정 및 퀵 헬프 주석 추가

--- Memento-iOS/Memento-iOS/Source/Extension/View+.swift
+++ Memento-iOS/Memento-iOS/Source/Extension/View+.swift
@@ -19,8 +19,20 @@ extension View {
         return self
     }
 
+    /// 태그 개수에 따라 시트의 동작과 크기를 동적으로 조정하는 메서드
+    ///
+    /// - 태그 개수가 4개를 초과하면 시트는 스크롤이 가능하며 두 가지 높이(`0.33`, `0.99`)로 조정
+    /// - 태그 개수가 4개 이하일 경우, 고정된 높이(`0.33`)로만 표시되며 스크롤 비활성화
+    ///
+    /// 사용 예시:
+    /// ```
+    /// ExampleView()
+    ///     .applyDynamicSheetForTagCount()
+    /// ```
+    ///
+    /// - Returns: 동적으로 동작하는 시트 반환
     @ViewBuilder
-    func applyDynamicSheetBehavior() -> some View {
+    func applyDynamicSheetForTagCount() -> some View {
         if Tag.mockData.count > 4 {
             self
                 .presentationContentInteraction(.scrolls)
