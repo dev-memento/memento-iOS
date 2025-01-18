89f0611fd1d18519e0ebe23e0f8836a6ea55ea73
RAFA <rafa71802937@gmail.com>
2025-01-18 18:50:35 +0900

    [Refactor] #51 - 코드 줄 정렬 및 애니메이션 제거

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddSchedule/View/RepeatPickerView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddSchedule/View/RepeatPickerView.swift
@@ -12,88 +12,94 @@ import MDSKit
 struct RepeatPickerView: View {
 
     // MARK: - Properties
+    
+    @ObservedObject var viewModel: PickerButtonViewModel
+    @StateObject private var repeatViewModel = PickerButtonViewModel(type: .repeat)
+    @StateObject private var endRepeatViewModel = PickerButtonViewModel(type: .endRepeat)
 
-   @ObservedObject var viewModel: PickerButtonViewModel
-   @StateObject private var repeatViewModel = PickerButtonViewModel(type: .repeat)
-   @StateObject private var endRepeatViewModel = PickerButtonViewModel(type: .endRepeat)
+    // MARK: - Body
 
-   var body: some View {
-       VStack(spacing: 14) {
-           HStack {
-               Text("Repeat")
-                   .applyFont(.body_r_16)
-                   .foregroundStyle(Color.gray05)
-               Spacer()
+    var body: some View {
+        VStack(spacing: 14) {
+            HStack {
+                Text("Repeat")
+                    .applyFont(.body_r_16)
+                    .foregroundStyle(Color.gray05)
+                Spacer()
 
-               PickerButton(
-                   type: .repeat,
-                   title: viewModel.repeatType.title,
-                   titleColor: .gray02,
-                   width: 200,
-                   action: { repeatViewModel.togglePresentation() },
-                   viewModel: repeatViewModel
-               )
-               .sheet(
-                   isPresented: $repeatViewModel.isPresented,
-                   onDismiss: { repeatViewModel.dismiss() }
-               ) {
-                   SheetContainer(height: 350) {
-                       VStack {
-                           SheetHeaderView {
-                               viewModel.updateRepeatType(repeatViewModel.repeatType)
-                               repeatViewModel.confirmSelection()
-                           }
+                PickerButton(
+                    type: .repeat,
+                    title: viewModel.repeatType.title,
+                    titleColor: .gray02,
+                    width: 200,
+                    action: { repeatViewModel.togglePresentation() },
+                    viewModel: repeatViewModel
+                )
+                .sheet(
+                    isPresented: $repeatViewModel.isPresented,
+                    onDismiss: { repeatViewModel.dismiss() }
+                ) {
+                    SheetContainer(height: 350) {
+                        VStack {
+                            SheetHeaderView {
+                                viewModel.updateRepeatType(repeatViewModel.repeatType)
+                                repeatViewModel.confirmSelection()
+                            }
 
-                           RepeatTypeListView(viewModel: repeatViewModel)
-                       }
-                   }
-               }
-           }
+                            RepeatTypeListView(viewModel: repeatViewModel)
+                        }
+                    }
+                }
+            }
 
-           if viewModel.shouldShowEndRepeat {
-               HStack {
-                   Text("End Repeat")
-                       .applyFont(.body_r_16)
-                       .foregroundStyle(Color.gray05)
-                   Spacer()
+            if viewModel.shouldShowEndRepeat {
+                HStack {
+                    Text("End Repeat")
+                        .applyFont(.body_r_16)
+                        .foregroundStyle(Color.gray05)
+                    Spacer()
 
-                   PickerButton(
-                       type: .endRepeat,
-                       title: viewModel.endRepeatDate?.formattedDate(with: "MMM d, yyyy") ?? "Select Date",
-                       titleColor: viewModel.endRepeatDate == nil ? .mementoBlue : .gray02,
-                       width: 200,
-                       action: { endRepeatViewModel.togglePresentation() },
-                       viewModel: endRepeatViewModel
-                   )
-                   .sheet(
-                       isPresented: $endRepeatViewModel.isPresented,
-                       onDismiss: { endRepeatViewModel.dismiss() }
-                   ) {
-                       BaseSheetView(
-                           selection: Binding(
-                               get: { viewModel.endRepeatDate ?? Date() },
-                               set: { viewModel.updateEndRepeatDate($0) }
-                           ),
-                           isPresented: $endRepeatViewModel.isPresented,
-                           type: .date,
-                           minimumDate: Date(),
-                           onDismiss: viewModel.confirmSelection
-                       )
-                   }
-               }
-               .transition(.opacity)
-               .animation(.easeInOut, value: viewModel.shouldShowEndRepeat)
-           }
-       }
-       .padding(.top)
-   }
+                    PickerButton(
+                        type: .endRepeat,
+                        title: viewModel.endRepeatDate?.formattedDate(
+                            with: "MMM d, yyyy"
+                        ) ?? "Select Date",
+                        titleColor: viewModel.endRepeatDate == nil ? .mementoBlue : .gray02,
+                        width: 200,
+                        action: { endRepeatViewModel.togglePresentation()
+                        },
+                        viewModel: endRepeatViewModel
+                    )
+                    .sheet(
+                        isPresented: $endRepeatViewModel.isPresented,
+                        onDismiss: { endRepeatViewModel.dismiss() }
+                    ) {
+                        BaseSheetView(
+                            selection: Binding(
+                                get: { viewModel.endRepeatDate ?? Date() },
+                                set: { viewModel.updateEndRepeatDate($0) }
+                            ),
+                            isPresented: $endRepeatViewModel.isPresented,
+                            type: .date,
+                            minimumDate: Date(),
+                            onDismiss: viewModel.confirmSelection
+                        )
+                    }
+                }
+                .transition(.opacity)
+            }
+        }
+        .padding(.top)
+    }
 }
 
-#Preview {
-   ZStack {
-       Color.gray10
-           .ignoresSafeArea()
+// MARK: - Preview
 
-       RepeatPickerView(viewModel: PickerButtonViewModel())
-   }
+#Preview {
+    ZStack {
+        Color.gray10
+            .ignoresSafeArea()
+
+        RepeatPickerView(viewModel: PickerButtonViewModel())
+    }
 }
