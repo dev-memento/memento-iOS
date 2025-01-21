82419d3dac7695a5aa428ab2248afa217dd71649
Gahyun Kim <mcrkgus@naver.com>
2025-01-21 22:52:43 +0900

    [Chore] #66 - initial durationTime

--- Memento-iOS/Memento-iOS/Source/Network/Schedule/DTO/Response/DailyScheduleData.swift
+++ Memento-iOS/Memento-iOS/Source/Network/Schedule/DTO/Response/DailyScheduleData.swift
@@ -5,6 +5,8 @@
 //  Created by Kimgahyun on 1/21/25.
 //
 
+import Foundation
+
 struct DailyScheduleData: Codable {
     let id: Int
     let description: String
@@ -14,4 +16,15 @@ struct DailyScheduleData: Codable {
     let tagColor: String
     let tagName: String
     let scheduleType: String
+    
+    init(id: Int, description: String, startDate: String, endDate: String, tagColor: String, tagName: String, scheduleType: String) {
+        self.id = id
+        self.description = description
+        self.startDate = startDate
+        self.endDate = endDate
+        self.tagColor = tagColor
+        self.tagName = tagName
+        self.scheduleType = scheduleType
+        self.durationTime = Date.calculateDuration(startDate: startDate, endDate: endDate)
+    }
 }
