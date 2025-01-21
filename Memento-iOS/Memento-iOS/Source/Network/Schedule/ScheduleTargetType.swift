1c76816928f1d30dae831b21bb0ed92b2313e8f7
Gahyun Kim <mcrkgus@naver.com>
2025-01-21 16:37:29 +0900

    [Network] #66 - Schedule target type

---
+++ Memento-iOS/Memento-iOS/Source/Network/Schedule/ScheduleTargetType.swift
@@ -0,0 +1,44 @@
+//
+//  ScheduleTargetType.swift
+//  Memento-iOS
+//
+//  Created by Kimgahyun on 1/21/25.
+//
+
+import Foundation
+
+import Moya
+
+enum ScheduleTargetType {
+    case getSchedulesTotal
+}
+
+extension ScheduleTargetType: BaseTargetType {
+    var headerType: HeaderType {
+        return .accessTokenHeader
+    }
+    
+    var utilPath: UtilPath {
+        return .schedule
+    }
+    
+    var pathParameter: String? {
+        return .none
+    }
+    
+    var queryParameter: [String: Any]? {
+        return .none
+    }
+    
+    var requestBodyParameter: Codable? {
+        return nil
+    }
+    
+    var path: String {
+        return utilPath.rawValue
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+}
