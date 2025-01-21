6640ec2e2816131fe106a3edc6da5c7694e349a4
RAFA <rafa71802937@gmail.com>
2025-01-21 20:39:53 +0900

    [Chore] #65 - 컴포넌트 분리

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoBottomView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoBottomView.swift
@@ -22,56 +22,72 @@ struct AddTodoBottomView: View {
 
     var body: some View {
         HStack(spacing: 8) {
-            Button(action: {
-                isDeadlinePresented.toggle()
-            }) {
-                HStack {
-                    Image(.ic_deadline)
-
-                    Text(pickerViewModel.formattedPickerTitle)
-                        .applyFont(.detail_r_12)
-                }
-                .frame(maxWidth: .infinity)
-                .foregroundStyle(Color.gray02)
-            }
-            .frame(width: 86, height: 41)
-            .background(Color.gray09)
-            .clipShape(RoundedRectangle(cornerRadius: 2))
-            .sheet(isPresented: $isDeadlinePresented) {
-                AddDeadlineView(
-                    viewModel: pickerViewModel,
-                    selectedDateText: $selectedDateText
-                )
-                .presentationDetents(
-                    DynamicPresentationDetent.dynamicDetent(for: .deadline)
-                )
-            }
-
-            Button(action: {}) {
-                Image(.ic_tag)
-                    .padding(14)
-                    .background(Color.gray09)
-                    .cornerRadius(2)
-            }
-            .frame(width: 42, height: 42)
-
-            Button(action: {}) {
-                Image(.matrix_none)
-                    .frame(width: 26, height: 26)
-                    .padding(8)
-                    .background(Color.gray09)
-                    .cornerRadius(2)
-            }
-            .frame(width: 42, height: 42)
-
+            deadlineButton
+            tagButton
+            matrixButton
             Spacer()
-
             enterButton
         }
         .padding(.bottom, 20)
     }
 
     // MARK: - UI Components
+
+    // MARK: Deadline Button
+
+    private var deadlineButton: some View {
+        Button(action: {
+            isDeadlinePresented.toggle()
+        }) {
+            HStack {
+                Image(.ic_deadline)
+
+                Text(pickerViewModel.formattedPickerTitle)
+                    .applyFont(.detail_r_12)
+            }
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(Color.gray02)
+        }
+        .frame(width: 86, height: 41)
+        .background(Color.gray09)
+        .clipShape(RoundedRectangle(cornerRadius: 2))
+        .sheet(isPresented: $isDeadlinePresented) {
+            AddDeadlineView(
+                viewModel: pickerViewModel,
+                selectedDateText: $selectedDateText
+            )
+            .presentationDetents(
+                DynamicPresentationDetent.dynamicDetent(for: .deadline)
+            )
+        }
+    }
+
+    // MARK: Tag Button
+
+    private var tagButton: some View {
+        Button(action: {}) {
+            Image(.ic_tag)
+                .padding(14)
+                .background(Color.gray09)
+                .cornerRadius(2)
+        }
+        .frame(width: 42, height: 42)
+    }
+
+    // MARK: Matrix Button
+
+    private var matrixButton: some View {
+        Button(action: {}) {
+            Image(.matrix_none)
+                .frame(width: 26, height: 26)
+                .padding(8)
+                .background(Color.gray09)
+                .cornerRadius(2)
+        }
+        .frame(width: 42, height: 42)
+    }
+
+    // MARK: Enter Button
 
     private var enterButton: some View {
         Button(action: {
