fcc0a3112f67095ae8dbfa06d6e3cf866ea4015d
RAFA <rafa71802937@gmail.com>
2025-01-22 05:33:24 +0900

    [Feat] #72 - 투두 추가 뷰에 사용되는 피커 버튼 뷰 모델 구현

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/ViewModel/AddTodoPickerButtonViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  AddTodoPickerButtonViewModel.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 1/22/25.
+//
+
+import SwiftUI
+
+import MDSKit
+
+final class AddTodoPickerButtonViewModel: BasePickerViewModel {
+    
+    // MARK: - Properties
+    
+    @Published var selectedDate: Date = Date()
+    @Published var selectedTag: Tag = Tag.mockData.first ?? Tag(color: .gray02, title: "Untitled")
+
+    let pickerType: AddTodoPickerButtonType
+
+    // MARK: - Initializer
+
+    init(type: AddTodoPickerButtonType = .tag) {
+        self.pickerType = type
+        super.init()
+    }
+
+    // MARK: - Computed properties
+
+    var formattedPickerTitle: String {
+        switch pickerType {
+        case .tag:
+            return selectedTag.title.isEmpty ? "Untitled" : selectedTag.title
+        case .deadline:
+            return Calendar.current.isDateInToday(selectedDate)
+                ? "Today"
+                : selectedDate.formattedDate(with: "MMM d")
+        }
+    }
+
+    // MARK: - Override methods
+
+    override func dismiss() {
+        super.dismiss()
+        setPressedState(false)
+    }
+}
