cb5e3edb497e12665f39f72016c2e34e41d594d9
RAFA <rafa71802937@gmail.com>
2025-01-22 06:09:22 +0900

    [Feat] #72 - 태그 뷰 구현 및 적용

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoBottomView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoBottomView.swift
@@ -15,6 +15,7 @@ struct AddTodoBottomView: View {
 
     @ObservedObject var viewModel: AddTodoTextViewModel
     @State private var isDeadlinePresented: Bool = false
+    @State private var isTagPresented: Bool = false
     @State private var selectedDateText: String = "Today"
     @State private var selectedTagColor: Color = .gray05
     @StateObject private var deadlineViewModel = AddTodoPickerButtonViewModel(type: .deadline)
@@ -42,7 +43,7 @@ struct AddTodoBottomView: View {
             HStack {
                 Image(.ic_deadline)
 
-                Text(pickerViewModel.formattedPickerTitle)
+                Text(deadlineViewModel.formattedPickerTitle)
                     .applyFont(.detail_r_12)
             }
             .frame(maxWidth: .infinity)
@@ -53,7 +54,7 @@ struct AddTodoBottomView: View {
         .clipShape(RoundedRectangle(cornerRadius: 2))
         .sheet(isPresented: $isDeadlinePresented) {
             AddDeadlineView(
-                viewModel: pickerViewModel,
+                viewModel: deadlineViewModel,
                 selectedDateText: $selectedDateText
             )
             .presentationDetents(
@@ -63,13 +64,16 @@ struct AddTodoBottomView: View {
     }
 
     private var tagButton: some View {
-        Button(action: {}) {
-            Image(.ic_tag)
-                .padding(14)
-                .background(Color.gray09)
-                .cornerRadius(2)
+        Button(action: {
+            isTagPresented.toggle()
+        }) {
+            Circle()
+                .fill(Color(tagViewModel.selectedTag.color))
+                .frame(width: 10, height: 10)
         }
         .frame(width: 42, height: 42)
+        .background(Color.gray09)
+        .clipShape(RoundedRectangle(cornerRadius: 2))
         .sheet(isPresented: $isTagPresented) {
             AddTagView(viewModel: tagViewModel)
                 .presentationDetents(
