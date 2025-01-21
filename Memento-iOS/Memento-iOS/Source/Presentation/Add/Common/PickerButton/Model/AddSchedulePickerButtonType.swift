70b52a791ab3b55d26c69ec9fca3956ada911f2f
RAFA <rafa71802937@gmail.com>
2025-01-22 05:31:52 +0900

    [Chore] #72 - 피커 버튼 타입 확장

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/Common/PickerButton/Model/AddSchedulePickerButtonType.swift
@@ -0,0 +1,16 @@
+//
+//  PickerButtonType.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 1/18/25.
+//
+
+import Foundation
+
+enum AddSchedulePickerButtonType {
+    case date, time, tag
+}
+
+enum AddTodoPickerButtonType {
+    case tag, deadline
+}
