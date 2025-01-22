c8afeb0456969db97de89d79eb1b6450999e4fda
RAFA <rafa71802937@gmail.com>
2025-01-22 17:09:16 +0900

    [Feat] #74 - 날짜 포맷팅 속성 추가

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/ViewModel/AddTodoHeaderViewModel.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/ViewModel/AddTodoHeaderViewModel.swift
@@ -11,4 +11,14 @@ final class AddTodoHeaderViewModel: ObservableObject {
 
     @Published var showDatePicker: Bool = false
     @Published var selectedDate: Date = Date()
+
+    var formattedDate: String {
+        if Calendar.current.isDateInToday(selectedDate) {
+            return "Today"
+        } else {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMM d"
+            return dateFormatter.string(from: selectedDate)
+        }
+    }
 }
