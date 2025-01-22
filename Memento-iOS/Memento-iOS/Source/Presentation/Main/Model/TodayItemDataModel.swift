b82c369f94faa57518333fea62c951ce02875b33
Gahyun Kim <mcrkgus@naver.com>
2025-01-23 02:04:29 +0900

    [Chore] #71 - today item data model - case schedule

--- Memento-iOS/Memento-iOS/Source/Presentation/Main/Model/TodayItemDataModel.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Main/Model/TodayItemDataModel.swift
@@ -7,19 +7,37 @@
 
 import Foundation
 
-enum TodayItemDataModel: Identifiable, Equatable {
+enum TodayItemDataModel: Identifiable {
     case todo(ToDoListDataModel)
-    case schedule(ScheduleListDataModel)
+    case schedule(ScheduleTotalResponseData)
     
-    var id: UUID {
+    var id: Int {
         switch self {
         case .todo(let todo):
-            return todo.id
+            return todo.id.hashValue
         case .schedule(let schedule):
             return schedule.id
         }
     }
 }
+
+//extension TodayItemDataModel {
+//    var toDoBinding: ToDoListDataModel {
+//        if case .todo(let todo) = self {
+//            return todo
+//        } else {
+//            fatalError("not To Do case")
+//        }
+//    }
+//    
+//    var scheduleBinding: ScheduleTotalResponseData {
+//        if case .schedule(let schedule) = self {
+//            return schedule
+//        } else {
+//            fatalError("not Schedule case")
+//        }
+//    }
+//}
 
 extension TodayItemDataModel {
     var toDoBinding: ToDoListDataModel {
@@ -34,6 +52,20 @@ extension TodayItemDataModel {
             if case .todo = self {
                 self = .todo(newValue)
             }
+        }
+    }
+}
+
+
+extension TodayItemDataModel: Equatable {
+    static func == (lhs: TodayItemDataModel, rhs: TodayItemDataModel) -> Bool {
+        switch (lhs, rhs) {
+        case (.todo(let lhsTodo), .todo(let rhsTodo)):
+            return lhsTodo == rhsTodo
+        case (.schedule(let lhsSchedule), .schedule(let rhsSchedule)):
+            return lhsSchedule == rhsSchedule
+        default:
+            return false
         }
     }
 }
