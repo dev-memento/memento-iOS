e64cfd75763e42202a6f12a84db0efd6acbe8643
RAFA <rafa71802937@gmail.com>
2025-01-23 12:50:10 +0900

    [Feat] #76 - 토큰 불러오는 메서드 구현

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoView.swift
@@ -43,9 +43,27 @@ struct AddTodoView: View {
         .padding(.horizontal)
         .background(Color.gray10)
         .onAppear {
-}
+            loadTokens()
             todoViewModel.startDate = headerViewModel.isoFormattedDate
             todoViewModel.endDate = bottomViewModel.isoFormattedDate
         }
     }
 
+    private func loadTokens() {
+        do {
+            if let accessToken = try TokenKeychainManager.shared.loadAccessToken() {
+                print("Access Token: \(accessToken)")
+            } else {
+                print("Access Token이 저장되지 않았습니다.")
+            }
+
+            if let refreshToken = try TokenKeychainManager.shared.loadRefreshToken() {
+                print("Refresh Token: \(refreshToken)")
+            } else {
+                print("Refresh Token이 저장되지 않았습니다.")
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+}
