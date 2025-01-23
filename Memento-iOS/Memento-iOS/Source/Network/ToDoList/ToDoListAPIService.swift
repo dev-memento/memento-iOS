72cbf3325295eed4091d54b460445ec159d67fd2
sem-git <lsmsm0327@naver.com>
2025-01-23 14:36:03 +0900

    [Network] #82 - ToDoListAPIService

---
+++ Memento-iOS/Memento-iOS/Source/Network/ToDoList/ToDoListAPIService.swift
@@ -0,0 +1,43 @@
+//
+//  ToDoListAPIService.swift
+//  Memento-iOS
+//
+//  Created by 이세민 on 1/23/25.
+//
+
+import Foundation
+import Moya
+
+// MARK: - ToDoListAPIServiceProtocol
+
+protocol ToDoListAPIServiceProtocol {
+    func getToDoList(completion: @escaping (NetworkResult<ToDoListResponseDTO>) -> Void)
+}
+
+extension ToDoListAPIServiceProtocol {
+    typealias ToDoListResponseDTO = BaseDTO<[ToDoListResponseData]>
+}
+
+// MARK: - ToDoListAPIService
+
+final class ToDoListAPIService: BaseAPIService, ToDoListAPIServiceProtocol {
+    
+    private let provider = MoyaProvider<ToDoListTargetType>(plugins: [MoyaPlugin.shared])
+    
+    // To-Do List API 연결
+    func getToDoList(completion: @escaping (NetworkResult<ToDoListResponseDTO>) -> Void) {
+        provider.request(.getToDoList) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<ToDoListResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                print(networkResult.stateDescription)
+                completion(networkResult)
+            case .failure(let error):
+                if let response = error.response {
+                    let networkResult: NetworkResult<ToDoListResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                    completion(networkResult)
+                }
+            }
+        }
+    }
+}
