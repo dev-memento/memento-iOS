f0ee71563afd191bae1cd1df98e5877a04d6f82e
sem-git <lsmsm0327@naver.com>
2025-01-24 08:17:20 +0900

    [Feat] #82 - 매핑 함수 구현

--- Memento-iOS/Memento-iOS/Source/Presentation/Main/Model/ToDoListDataModel.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Main/Model/ToDoListDataModel.swift
@@ -7,11 +7,26 @@
 
 import Foundation
 
-struct ToDoListDataModel: Identifiable, Equatable {
+struct ToDoListDataModel: Identifiable, Equatable, Hashable {
     var id = Int()
     var colorType: String
     var toDoTitle: String
     var dueDate: String
     var priorityType: Priority
     var isChecked: Bool
+    
+    func mapToToDoItem() -> ToDoListTotalResponseDataTest {
+        return .init(id: id,
+                     groupId: "",
+                     description: "",
+                     startDate: "",
+                     endDate: "",
+                     isCompleted: false,
+                     priorityValue: 1,
+                     priorityType: priorityType.title,
+                     tagName: "",
+                     tagColor: colorType,
+                     toDoType: "",
+                     orderNum: 0)
+    }
 }
