03eb13258a5c14688fed1fd6fdf5442f9798e2da
Gahyun Kim <mcrkgus@naver.com>
2025-01-23 10:52:08 +0900

    [Network] #84 - set getTags API

--- Memento-iOS/Memento-iOS/Source/Network/Tag/TagAPIService.swift
+++ Memento-iOS/Memento-iOS/Source/Network/Tag/TagAPIService.swift
@@ -12,11 +12,11 @@ import Moya
 // MARK: - TagAPIServiceProtocol
 
 protocol TagAPIServiceProtocol {
-
+    func getTags(completion: @escaping (NetworkResult<TagResponseDTO>) -> Void)
 }
 
 extension TagAPIServiceProtocol {
-
+    typealias TagResponseDTO = BaseDTO<TagResponseData>
 }
 
 // MARK: - TagAPIService
@@ -24,5 +24,19 @@ extension TagAPIServiceProtocol {
 final class TagAPIService: BaseAPIService, TagAPIServiceProtocol {
 
     private let provider = MoyaProvider<TagTargetType>(plugins: [MoyaPlugin.shared])
+    
+    func getTags(completion: @escaping (NetworkResult<TagResponseDTO>) -> Void) {
+        provider.request(.getTags) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<TagResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                print(networkResult.stateDescription)
+                completion(networkResult)
+            case .failure(let error):
+                if let response = error.response {
+                    let networkResult: NetworkResult<TagResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                }
+            }}
+    }
     
 }
