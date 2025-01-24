af73d574a2e30c62b629a2623e0bbc6e7cf9f53c
sem-git <lsmsm0327@naver.com>
2025-01-24 18:18:01 +0900

    [Network] #82 - ToDoList 완료상태 수정 ResponseData 추가

---
+++ Memento-iOS/Memento-iOS/Source/Network/ToDoList/DTO/Response/ToDoListCompletedResponseData.swift
@@ -0,0 +1,11 @@
+//
+//  ToDoListCompletedResponseData.swift
+//  Memento-iOS
+//
+//  Created by 이세민 on 1/24/25.
+//
+
+struct ToDoListCompletedResponseData: Codable {
+    let id: Int
+    let isCompleted: Bool
+}
