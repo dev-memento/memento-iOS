9a52d16a25ad1014de4e33990f911f89aee4a31c
Gahyun Kim <mcrkgus@naver.com>
2025-01-24 15:11:44 +0900

    [Chore] #99 - type fix
    TODO: - refactor

--- Memento-iOS/Memento-iOS/Source/Network/Tag/TagAPIService.swift
+++ Memento-iOS/Memento-iOS/Source/Network/Tag/TagAPIService.swift
@@ -12,7 +12,7 @@ import Moya
 // MARK: - TagAPIServiceProtocol
 
 protocol TagAPIServiceProtocol {
-    func getTags(completion: @escaping (NetworkResult<TagResponseDTO>) -> Void)
+    func getTags(completion: @escaping (NetworkResult<BaseDTO<[TagResponseData]>>) -> Void)
 }
 
 extension TagAPIServiceProtocol {
@@ -25,18 +25,19 @@ final class TagAPIService: BaseAPIService, TagAPIServiceProtocol {
 
     private let provider = MoyaProvider<TagTargetType>(plugins: [MoyaPlugin.shared])
     
-    func getTags(completion: @escaping (NetworkResult<TagResponseDTO>) -> Void) {
+    func getTags(completion: @escaping (NetworkResult<BaseDTO<[TagResponseData]>>) -> Void) {
         provider.request(.getTags) { result in
             switch result {
             case .success(let response):
-                let networkResult: NetworkResult<TagResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                let networkResult: NetworkResult<BaseDTO<[TagResponseData]>> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                 print(networkResult.stateDescription)
                 completion(networkResult)
             case .failure(let error):
                 if let response = error.response {
-                    let networkResult: NetworkResult<TagResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                    let networkResult: NetworkResult<BaseDTO<[TagResponseData]>> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                 }
-            }}
+            }
+        }
     }
     
 }
