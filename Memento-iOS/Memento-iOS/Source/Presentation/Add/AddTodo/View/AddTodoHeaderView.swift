9e0b0ea6f586c13dc17fd0053655eeafd31d66ac
RAFA <rafa71802937@gmail.com>
2025-02-15 20:29:44 +0900

    [Refactor] #1 - 불필요한 코드 제거

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoHeaderView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/AddTodo/View/AddTodoHeaderView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 import MDSKit
 
 struct AddTodoHeaderView: View {
-    
+
     // MARK: - Properties
-    
+
     @ObservedObject var viewModel: AddTodoViewModel
 
     // MARK: - Body
-    
+
     var body: some View {
         HStack {
             Text("Add to-do,")
                 .foregroundColor(.gray07)
                 .applyFont(.body_b_18)
-            
+
             Button(viewModel.formattedStartDate) {
                 viewModel.showStartDatePicker = true
             }
@@ -30,24 +30,22 @@ struct AddTodoHeaderView: View {
             .applyFont(.body_b_18)
             .sheet(isPresented: $viewModel.showStartDatePicker) {
                 SheetContainer(type: .addTodo(.date)) {
-                    VStack {
-                        SheetHeaderView {
-                            viewModel.showStartDatePicker = false
-                        }
-                        
-                        DatePicker(
-                            "",
-                            selection: $viewModel.startDate,
-                            displayedComponents: .date
-                        )
-                        .colorScheme(.dark)
-                        .datePickerStyle(.graphical)
-                        .transition(.move(edge: .bottom))
-                        .tint(.mementoBlue)
+                    SheetHeaderView {
+                        viewModel.showStartDatePicker = false
                     }
+
+                    DatePicker(
+                        "",
+                        selection: $viewModel.startDate,
+                        displayedComponents: .date
+                    )
+                    .colorScheme(.dark)
+                    .datePickerStyle(.graphical)
+                    .tint(.mementoBlue)
+                    .padding([.horizontal, .bottom], 20)
                 }
             }
-            
+
             Spacer()
         }
     }
