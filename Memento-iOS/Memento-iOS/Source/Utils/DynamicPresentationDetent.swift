c9f638b7838d46b598029a952dde346edcdf9faa
RAFA <rafa71802937@gmail.com>
2025-03-23 22:30:11 +0900

    [Refactor] #15 - 동적 바텀 시트 설정 로직 간소화 및 개선

--- Memento-iOS/Memento-iOS/Source/Utils/DynamicPresentationDetent.swift
+++ Memento-iOS/Memento-iOS/Source/Utils/DynamicPresentationDetent.swift
@@ -10,68 +10,78 @@ import SwiftUI
 /// 기기 크기 카테고리
 private enum DeviceSizeCategory {
     case small, medium, large
+
+    static var currentDeviceSize: DeviceSizeCategory {
+        let height = UIScreen.main.bounds.height
+
+        switch height {
+        case ..<812   : return .small  // SE
+        case 812..<875: return .medium // mini, pro
+        default       : return .large  // plus, max
+        }
+    }
 }
 
 /// 화면 크기에 따라 동적으로 시트 높이를 조정하는 유틸리티
 struct DynamicPresentationDetent {
 
-    static let smallDeviceRange = CGFloat(0)..<CGFloat(668)    // 작은 기기
-    static let mediumDeviceRange = CGFloat(668)..<CGFloat(875) // 중간 기기
-    static let largeDeviceRange = CGFloat(875)..<CGFloat(957)  // 대형 기기
-
-    /// 기기 화면 크기에 따라 Detent 높이를 계산
+    /// 기기 화면 크기에 따라 바텀 시트의 높이를 계산
     static func dynamicDetent(for type: PickerButtonType) -> Set<PresentationDetent> {
-        let screenHeight = UIScreen.main.bounds.height
-        let detentHeight = detentHeight(for: type, deviceSize: screenHeight)
-        return [.fraction(detentHeight)]
+        let device = DeviceSizeCategory.currentDeviceSize
+        return [.fraction(fraction(for: type, on: device))]
     }
 
-    /// Detent 높이를 결정하는 함수
-    private static func detentHeight(for type: PickerButtonType, deviceSize: CGFloat) -> CGFloat {
-        let heightCategory = categorizeDeviceSize(deviceSize)
-
-        switch type {
-        case .addTodo(let todoType):
-            return heightFor(todoType, in: heightCategory)
-        case .addSchedule(let scheduleType):
-            return heightFor(scheduleType, in: heightCategory)
-        }
-    }
-
-    /// 기기의 화면 크기에 따라 카테고리 결정
-    private static func categorizeDeviceSize(_ size: CGFloat) -> DeviceSizeCategory {
-        switch size {
-        case smallDeviceRange: return .small
-        case mediumDeviceRange: return .medium
-        case largeDeviceRange: return .large
-        default: return .large
-        }
-    }
-
-    /// 화면 크기별 Detent 높이 설정
-    private static func heightFor(
-        _ type: AddTodoPickerButtonType,
-        in category: DeviceSizeCategory
+    private static func fraction(
+        for type: PickerButtonType,
+        on device: DeviceSizeCategory
     ) -> CGFloat {
-        switch (type, category) {
-        case (.date, .small): return 0.65
-        case (.date, .medium): return 0.58
-        case (.date, .large): return 0.55
+        switch type {
+        case .addTodo(let subtype):
+            return fractionForAddTodo(subtype: subtype, device: device)
+        case .addSchedule(let subtype):
+            return fractionForAddSchedule(subtype: subtype, device: device)
+        }
+    }
+
+    private static func fractionForAddTodo(
+        subtype: AddTodoPickerButtonType,
+        device: DeviceSizeCategory
+    ) -> CGFloat {
+        switch subtype {
+        case .date:
+            switch device {
+            case .small : return AddTodoDateFraction.small
+            case .medium: return AddTodoDateFraction.medium
+            case .large : return AddTodoDateFraction.large
+            }
+        case .priority:
+            switch device {
+            case .small : return AddTodoPriorityFraction.small
+            case .medium: return AddTodoPriorityFraction.medium
+            case .large : return AddTodoPriorityFraction.large
+            }
         default: return 0
         }
     }
 
-    private static func heightFor(
-        _ type: AddSchedulePickerButtonType,
-        in category: DeviceSizeCategory
+    private static func fractionForAddSchedule(
+        subtype: AddSchedulePickerButtonType,
+        device: DeviceSizeCategory
     ) -> CGFloat {
-        switch (type, category) {
-        case (.date, .small): return 0.6
-        case (.date, .medium): return 0.53
-        case (.date, .large): return 0.5
-        case (.time, .small), (.tag, .small): return 0.43
-        case (.time, .medium), (.tag, .medium): return 0.36
-        case (.time, .large), (.tag, .large): return 0.33
+        switch subtype {
+        case .date:
+            switch device {
+            case .small : return AddScheduleDateFraction.small
+            case .medium: return AddScheduleDateFraction.medium
+            case .large : return AddScheduleDateFraction.large
+            }
+        case .time:
+            switch device {
+            case .small : return AddScheduleTimeFraction.small
+            case .medium: return AddScheduleTimeFraction.medium
+            case .large : return AddScheduleTimeFraction.large
+            }
+        default: return 0
         }
     }
 }
