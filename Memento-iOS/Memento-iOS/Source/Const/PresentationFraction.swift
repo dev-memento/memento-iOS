fb78273a69ce25230dc340b5c63d464e200d6cfd
RAFA <rafa71802937@gmail.com>
2025-03-23 22:29:02 +0900

    [Add] #15 - PresentationFraction 열거형 추가

---
+++ Memento-iOS/Memento-iOS/Source/Const/PresentationFraction.swift
@@ -0,0 +1,45 @@
+//
+//  PresentationFraction.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 3/23/25.
+//
+
+import Foundation
+
+enum PresentationFraction {
+
+    enum AddTodo {
+        enum Date {
+            static let small: CGFloat = 0.65
+            static let medium: CGFloat = 0.56
+            static let large: CGFloat = 0.52
+        }
+
+        enum Priority {
+            static let small: CGFloat = 0.83
+            static let medium: CGFloat = 0.8
+            static let large: CGFloat = 0.8
+        }
+    }
+
+    enum AddSchedule {
+        enum Date {
+            static let small: CGFloat = 0.65
+            static let medium: CGFloat = 0.56
+            static let large: CGFloat = 0.52
+        }
+
+        enum Time {
+            static let small: CGFloat = 0.43
+            static let medium: CGFloat = 0.37
+            static let large: CGFloat = 0.33
+        }
+    }
+}
+
+typealias AddTodoDateFraction     = PresentationFraction.AddTodo.Date
+typealias AddTodoPriorityFraction = PresentationFraction.AddTodo.Priority
+
+typealias AddScheduleDateFraction = PresentationFraction.AddSchedule.Date
+typealias AddScheduleTimeFraction = PresentationFraction.AddSchedule.Time
