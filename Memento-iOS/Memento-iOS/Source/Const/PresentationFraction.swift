0df44f01d192fae01a3e453c7018ab6e98537e45
RAFA <rafa71802937@gmail.com>
2025-03-24 12:36:37 +0900

    [Chore] #15 - 코드 정렬

--- Memento-iOS/Memento-iOS/Source/Const/PresentationFraction.swift
+++ Memento-iOS/Memento-iOS/Source/Const/PresentationFraction.swift
@@ -11,29 +11,29 @@ enum PresentationFraction {
 
     enum AddTodo {
         enum Date {
-            static let small: CGFloat = 0.65
+            static let small : CGFloat = 0.65
             static let medium: CGFloat = 0.56
-            static let large: CGFloat = 0.52
+            static let large : CGFloat = 0.52
         }
 
         enum Priority {
-            static let small: CGFloat = 0.83
+            static let small : CGFloat = 0.83
             static let medium: CGFloat = 0.8
-            static let large: CGFloat = 0.8
+            static let large : CGFloat = 0.8
         }
     }
 
     enum AddSchedule {
         enum Date {
-            static let small: CGFloat = 0.65
+            static let small : CGFloat = 0.65
             static let medium: CGFloat = 0.56
-            static let large: CGFloat = 0.52
+            static let large : CGFloat = 0.52
         }
 
         enum Time {
-            static let small: CGFloat = 0.43
+            static let small : CGFloat = 0.43
             static let medium: CGFloat = 0.37
-            static let large: CGFloat = 0.33
+            static let large : CGFloat = 0.33
         }
     }
 }
