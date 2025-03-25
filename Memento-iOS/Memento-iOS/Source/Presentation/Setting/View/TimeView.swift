ef6807eef301c43f2f340f0bd0842b64ce6cf13b
정정욱 <jeonguk29@naver.com>
2025-03-25 16:48:20 +0900

    [Feat] #16 - Setting TimeView 구현

---
+++ Memento-iOS/Memento-iOS/Source/Presentation/Setting/View/TimeView.swift
@@ -0,0 +1,98 @@
+//
+//  TimeView.swift
+//  Memento-iOS
+//
+//  Created by 정정욱 on 3/21/25.
+//
+
+import SwiftUI
+import MDSKit
+
+struct TimeView: View {
+    @EnvironmentObject var viewModel: SettingViewModel
+    @State private var isPickerPresented: Bool = false
+    @State private var selectedTimeType: TimeType = .wakeUp
+    
+    var body: some View {
+        VStack {
+            TimeSelectionView(
+                wakeUpTime: $viewModel.wakeUpTime,
+                windDownTime: $viewModel.sleepTime,
+                isPickerPresented: $isPickerPresented,
+                selectedTimeType: $selectedTimeType
+            )
+        }
+        .sheet(isPresented: $isPickerPresented) {
+            TimePickerView(
+                isPickerPresented: $isPickerPresented,
+                selectedTimeType: $selectedTimeType,
+                wakeUpTime: $viewModel.wakeUpTime,
+                windDownTime: $viewModel.sleepTime
+            )
+        }
+    }
+    
+}
+
+private struct TimeSelectionView: View {
+    @Binding var wakeUpTime: Date?
+    @Binding var windDownTime: Date?
+    @Binding var isPickerPresented: Bool
+    @Binding var selectedTimeType: TimeType
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 29) {
+            // 텍스트 수정 필요
+            timeSelectionRow(
+                icon: .ic_wakeup,
+                title: OnboardingSleepCycleText.wakeUpTitle,
+                time: wakeUpTime,
+                action: {
+                    selectedTimeType = .wakeUp
+                    isPickerPresented = true
+                }
+            )
+            
+            timeSelectionRow(
+                icon: .ic_winddown,
+                title: OnboardingSleepCycleText.windDownTitle,
+                time: windDownTime,
+                action: {
+                    selectedTimeType = .windDown
+                    isPickerPresented = true
+                }
+            )
+        }
+    }
+    
+    private func timeSelectionRow(icon: MDSImageName, title: String, time: Date?, action: @escaping () -> Void) -> some View {
+        HStack {
+            Image(icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 33, height: 33)
+                .foregroundColor(.white)
+            
+            Text(title)
+                .applyFont(.title_b_22)
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Button(action: action) {
+                Text(time.map { $0.formattedDate(with: "hh:mm a") } ?? OnboardingSleepCycleText.defaultTime)
+                    .applyFont(.body_r_14)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
+                    .background(Color.gray09)
+            }
+            .frame(width: 94, height: 36)
+        }
+    }
+}
+
+
+#Preview {
+    TimeView()
+}
