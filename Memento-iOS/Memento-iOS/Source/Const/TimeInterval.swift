c6a5ec24eddde93d27134ec5eb1e04cc4024a6be
RAFA <rafa71802937@gmail.com>
2025-04-06 21:27:09 +0900

    [Feat] #24 - TimeInterval 상수 파일 추가
    TimeInterval 확장을 통해 하루의 초를 나타내는 상수를 정의함

---
+++ Memento-iOS/Memento-iOS/Source/Const/TimeInterval.swift
@@ -0,0 +1,13 @@
+//
+//  TimeInterval.swift
+//  Memento-iOS
+//
+//  Created by RAFA on 4/6/25.
+//
+
+import Foundation
+
+extension TimeInterval {
+
+    static let secondsInADay: TimeInterval = 86_400
+}
