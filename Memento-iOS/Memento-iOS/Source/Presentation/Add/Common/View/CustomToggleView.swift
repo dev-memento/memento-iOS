5b902c73adec5edfae31ba3563ac5a08d2601d48
sem-git <lsmsm0327@naver.com>
2025-08-20 06:24:02 +0900

    [Refactor] #36 - CustomToggleView 단일화 및 불필요한 ToggleStyle 제거

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/Common/View/CustomToggleView.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/Common/View/CustomToggleView.swift
@@ -12,14 +12,28 @@ import MDSKit
 struct CustomToggleView: View {
     
     @Binding var isOn: Bool
-
+    
     var body: some View {
-        Toggle(isOn: $isOn) {
+        HStack {
             Text(StringLiteral.Common.toggleButtonTitle)
                 .applyFont(.detail_b_10)
                 .foregroundStyle(Color.gray07)
                 .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 12)
+                .fill(isOn ? Color.mainGreen : Color.gray08)
+                .animation(.easeInOut(duration: 0.2), value: isOn)
+                .frame(width: 40, height: 24)
+                .overlay(
+                    Circle()
+                        .fill(Color.gray02)
+                        .frame(width: 20, height: 20)
+                        .offset(x: isOn ? 8 : -8)
+                        .animation(.easeInOut(duration: 0.2), value: isOn)
+                )
+                .onTapGesture { isOn.toggle() }
         }
-        .toggleStyle(CustomToggleStyle())
     }
 }
