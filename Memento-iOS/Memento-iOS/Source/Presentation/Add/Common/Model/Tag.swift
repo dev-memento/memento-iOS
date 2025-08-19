f4f4bd4fbc384229606f306d8d92ff74779c391a
sem-git <lsmsm0327@naver.com>
2025-08-20 07:14:38 +0900

    [Del] #36 - Tag mockData 제거

--- Memento-iOS/Memento-iOS/Source/Presentation/Add/Common/Model/Tag.swift
+++ Memento-iOS/Memento-iOS/Source/Presentation/Add/Common/Model/Tag.swift
@@ -14,17 +14,11 @@ struct Tag: Equatable, Identifiable {
     let tagId: Int
     var name: String
     var color: Color
-   
+    
     init(id: UUID = UUID(), tagId: Int, name: String, color: Color) {
         self.id = id
         self.tagId = tagId
-        self.name =  name
+        self.name = name
         self.color = color
     }
-}
-
-extension Tag {
-    static let mockData: [Tag] = [
-        Tag(tagId: 1, name: "Untitled", color: .gray05)
-    ]
 }
