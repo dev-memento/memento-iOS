1e2d5e692db22e2669f3905c9347440982b4d603
Gahyun Kim <mcrkgus@naver.com>
2025-08-29 20:20:29 +0900

    [Feat] #42 - Tag Post API DTO Setting

---
+++ Memento-iOS/Memento-iOS/Source/Network/Tag/DTO/Request/TagPostRequest.swift
@@ -0,0 +1,13 @@
+//
+//  TagPostRequest.swift
+//  Memento-iOS
+//
+//  Created by Kimgahyun on 8/29/25.
+//
+
+import Foundation
+
+struct TagPostRequest: Codable {
+    let name: String
+    let hexCode: String
+}
---
+++ Memento-iOS/Memento-iOS/Source/Network/Tag/DTO/Response/TagPostResponse.swift
@@ -0,0 +1,13 @@
+//
+//  TagPostResponse.swift
+//  Memento-iOS
+//
+//  Created by Kimgahyun on 8/29/25.
+//
+
+import Foundation
+
+struct TagPostResponse: Codable {
+    let name: String
+    let colorCode: String
+}
