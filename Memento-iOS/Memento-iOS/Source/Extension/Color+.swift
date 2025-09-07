b48398a7061e5b88bd4f04c2a1874b3bc728a8e9
sem-git <lsmsm0327@naver.com>
2025-09-07 15:30:07 +0900

    [Feat] #44 - toHex 메서드 구현

--- Memento-iOS/Memento-iOS/Source/Extension/Color+.swift
+++ Memento-iOS/Memento-iOS/Source/Extension/Color+.swift
@@ -9,6 +9,20 @@ import SwiftUI
 
 extension Color {
     static func fromHex(_ hex: String) -> Color {
-       return  Color(hex: hex)
+        return  Color(hex: hex)
     }
+    
+    func toHex() -> String {
+            let uiColor = UIColor(self)
+            var r: CGFloat = 0
+            var g: CGFloat = 0
+            var b: CGFloat = 0
+            var a: CGFloat = 0
+
+            if !uiColor.getRed(&r, green: &g, blue: &b, alpha: &a) {
+                return "#000000"
+            }
+            let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)
+            return String(format:"#%06X", rgb)
+        }
 }
